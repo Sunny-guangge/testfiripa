3159d3c49d6f69ba119d67a0fcfe0c2fb2eefbb9
sunny <wangsg@bankhui.com>
2017-08-10 16:01:25 +0800

    测试

---
+++ testfiripa/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  testfiripa
+//
+//  Created by 王帅广 on 2017/8/10.
+//  Copyright © 2017年 王帅广. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
